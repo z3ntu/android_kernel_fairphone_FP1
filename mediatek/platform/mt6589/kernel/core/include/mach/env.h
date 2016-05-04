ad981273cf91d03879884fdbe17f32fc6ee51195
Daniel Calviño Sánchez <danxuliu@gmail.com>
2016-05-05 01:24:54 +0200

    Remove superfluous include and forward declaration from "mach/env.h".
    The "partition_define.h" header is only needed for "dum-char" and
    "mmc-host" drivers, so it is no longer included in the general
    "mediatek/platform/mt6589/kernel/core/include/mach/env.h" header.
    
    In the same way, the "eMMC_rw_x" function is only needed for "dum-char"
    driver, so it was also removed from the general "mach/env.h" header.

--- mediatek/platform/mt6589/kernel/core/include/mach/env.h
+++ mediatek/platform/mt6589/kernel/core/include/mach/env.h
@@ -1,6 +1,5 @@
 #ifndef __ENV_H__
 #define __ENV_H__
-#include "partition_define.h"
 #include <linux/errno.h>	/* error codes */
 #include <linux/types.h>	/* size_t */
 #include <linux/proc_fs.h>  /*proc*/
@@ -47,9 +46,6 @@ struct env_ioctl
 	int value_len;	
 };
 extern void env_init(loff_t env_part_addr);
-#ifdef MTK_EMMC_SUPPORT
-extern int eMMC_rw_x(loff_t addr,u32  *buffer, int host_num, int iswrite,u32 totalsize, int transtype, Region part);
-#endif
 extern int set_env(char *name,char *value);
 extern char *get_env(char *name);
 extern long long data_free_size_th;
