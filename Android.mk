0f665553cbbdcb1894b60775d5919f28599ae83c
Daniel Calviño Sánchez <danxuliu@gmail.com>
2016-05-23 17:36:04 +0200

    Prevent Android Build System from reading Android.mk files.
    The Android.mk files in the kernel tree are not needed to build the
    kernel, but some of them clash with other Android.mk used elsewhere
    in the Android tree. Therefore, an empty Android.mk file was added to
    prevent the Android Build System from finding other deeper Android.mk
    files.

---
+++ Android.mk
@@ -0,0 +1,5 @@
+# Empty file to prevent Android Build System from finding Android.mk files in
+# subdirectories. Those files are not needed and, moreover, some even clash with
+# the real ones to be used (for example,
+# "./mediatek/platform/mt6589/hardware/camera/device/Android.mk" clashes with
+# "<root>/hardware/libhardware/modules/camera/Android.mk").
